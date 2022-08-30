45a56eca4c4bf4abb0dc7e8d74ab65c2ad1eb565
ErickJValdez <erick.valdez.vargas@gmail.com>
2022-08-30 14:11:13 -0400

    Fix issues

--- force-app/main/default/lwc/uploadCustomFile/uploadCustomFile.js
+++ force-app/main/default/lwc/uploadCustomFile/uploadCustomFile.js
@@ -1,44 +1,73 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import uploadFile from '@salesforce/apex/CustomFileUploadController.uploadFile';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class UploadCustomFile extends LightningElement {
-    @track showSpinner = false;
-    @track fileData;
+    showSpinner = false;
     @api recordId;
-    @track fileName;
+    filesName;
+    @track filesData=[];
+    countOfItems;
+
      // getting file 
     handleFileChange(event) {
-        if(event.target.files.length > 0) {
-            const file = event.target.files[0]
-            var reader = new FileReader()
-            reader.onload = () => {
-                var base64 = reader.result.split(',')[1]
-                this.fileName = file.name;
-                this.fileData = {
-                    'filename': file.name,
-                    'base64': base64
-                }
-                console.log(this.fileData)
-            }
-            reader.readAsDataURL(file)
+        let files = event.target.files;
+        console.log('The files length is: '+files.length);
+        if(files.length > 0) {
+            let filesName = '';
+             for (let i = 0; i < files.length; i++) {
+                console.log('The iterator value is: '+i);
+                let file = files[i];
+
+                filesName = filesName + file.name + ',';
+
+                let reader = new FileReader();
+                reader.onload = () => {
+                    let base64 = 'base64,';
+                    let content = reader.result.indexOf(base64) + base64.length;
+                    let fileContents = reader.result.substring(content);
+                    
+                   this.countOfItems= this.filesData.push({
+                        fileName: file.name,
+                        versionData: fileContents,
+                        recordId: this.recordId
+                    }); 
+                };
+                reader.readAsDataURL(file);
+                console.log('The count of items is:'+ this.countOfItems);
+             }
+             this.filesName = filesName.slice(0, -1);
         }
+        console.log('The file data length in the handleFileChange is:'+ this.filesData.length);
+        console.log('The files to be uploaded names are: '+this.filesName);
+        console.log('The files data stringify objects are in the handleFileChange: '+ JSON.stringify(this.filesData));
+        files = null;
     }
  
-    uploadFile() {
-        this.handleSpinner();
-        const {base64, filename} = this.fileData
- 
-        uploadFile({ fileName:this.fileName, base64Data : base64, recordId:this.recordId }).then(result=>{
-            this.fileData = null
-            let title = `${filename} uploaded successfully!!`;
+    uploadFileHandler() {
+        this.handleSpinner(); 
+
+        console.log('The files data length inside the uploadfileHandler is: '+ this.filesData.length);
+        console.log('The files data stringify objects are in the uploadFileHandler: '+ JSON.stringify(this.filesData));
+
+        uploadFile({ filesToInsert: this.filesData}).then(result=>{
+            console.log(result);
+            this.filesData = null;
+            let title = this.filesName + 'uploaded successfully!!';
+            this.filesName= null;
             this.ShowToast('Success!', title, 'success', 'dismissable');
             this.updateRecordView(this.recordId);
         }).catch(err=>{
             this.ShowToast('Error!!', err.body.message, 'error', 'dismissable');
         }).finally(() => {
-            this.handleSpinner();
+          this.handleSpinner();
         })
+    
+    }
+
+    removeFile(event) {
+        var index = event.currentTarget.dataset.id;
+        this.filesData.splice(index, 1);
     }
  
     handleSpinner(){
